2642fba6e25ba55ed32ce9608c803872b49713f8
Sai Deepak <68103212+Saideepak01@users.noreply.github.com>
2021-11-29 07:17:45 +0530

    🌿

--- index.js
+++ index.js
@@ -11,63 +11,6 @@ dotenv.config();
 const app = express();
 const PORT = 9000;
 const MONGO_URL = process.env.MONGO_URL;
-const movies = [
-  {
-    id: "100",
-    name: "Interstellar",
-    poster: "https://i.ytimg.com/vi/uaSYEUugnzE/movieposter_en.jpg",
-    description:
-      "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
-    src: "https://www.youtube.com/embed/zSWdZVtXT7E",
-    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E",
-    imdb: 8.6,
-    language: "english",
-  },
-  {
-    id: "101",
-    name: "Inception",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg",
-    description:
-      "Cobb steals information from his targets by entering their dreams. Saito offers to wipe clean Cobb's criminal history as payment for performing an inception on his sick competitor's son.",
-    src: "https://www.youtube.com/embed/Qwe6qXFTdgc",
-    trailer: "https://www.youtube.com/embed/Qwe6qXFTdgc",
-    imdb: 8.8,
-    language: "english",
-  },
-  {
-    id: "102",
-    name: "Dunkirk",
-    poster: "https://m.media-amazon.com/images/I/91a9Ez60pmL._AC_SL1500_.jpg",
-    description:
-      "During World War II, soldiers from the British Empire, Belgium and France try to evacuate from the town of Dunkirk during a arduous battle with German forces.",
-    src: "https://www.youtube.com/embed/F-eMt3SrfFU",
-    imdb: 7.8,
-    language: "english",
-  },
-  {
-    id: "103",
-    name: "Tenet",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BNDhiODUyN2UtYTQzZi00YTE0LWJiMWEtYTVlODEyZDQwYzRhXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg",
-    description:
-      "When a few objects that can be manipulated and used as weapons in the future fall into the wrong hands, a CIA operative, known as the Protagonist, must save the world.",
-    src: "https://www.youtube.com/embed/AZGcmvrTX9M",
-    imdb: 7.4,
-    language: "telugu",
-  },
-  {
-    id: "104",
-    name: "Free guy",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTY2NzFjODctOWUzMC00MGZhLTlhNjMtM2Y2ODBiNGY1ZWRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
-    description:
-      "When a bank teller discovers he's actually a background player in an open-world video game, he decides to become the hero of his own story -- one that he can rewrite himself. In a world where there's no limits, he's determined to save the day his way before it's too late, and maybe find a little romance with the coder who conceived him.",
-    src: "https://www.youtube.com/embed/X2m-08cOAbc",
-    imdb: 7.2,
-    language: "tamil",
-  },
-];
 
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
